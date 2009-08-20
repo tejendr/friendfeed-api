f60ae9f79b063f130f35c0d06bd5b6632b0ecf70
benjamin.golub <benjamin.golub@740053e7-d448-0410-8e6a-8573859296cf>
2009-08-20 19:09:41 +0000

    fix link

--- trunk/v2/python/friendfeed.py
+++ trunk/v2/python/friendfeed.py
@@ -74,7 +74,7 @@ class FriendFeed(object):
         some feeds and to post messages, you must provide both
         oauth_consumer_token and oauth_access_token. They should both be
         dictionaries of the form {"key": "...", "secret": "..."}. Learn
-        more about OAuth at http://friendfeed.com/api/oauth.
+        more about OAuth at http://friendfeed.com/api/documentation#oauth.
 
         You can register your application to receive your FriendFeed OAuth
         Consumer Key at http://friendfeed.com/api/register. To fetch request
